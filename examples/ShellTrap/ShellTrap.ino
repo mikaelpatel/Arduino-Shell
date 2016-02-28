430b91e0014c627fcf116bc5a6978e3121be40c2
mikaelpatel <mikael.patel@gmail.com>
2016-02-28 01:08:54 +0100

    Add extended shell example sketch with instruction trap.

---
+++ examples/ShellTrap/ShellTrap.ino
@@ -0,0 +1,58 @@
+/**
+ * @file ShellTrap.ino
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2016, Mikael Patel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * @section Description
+ * This Arduino sketch shows how to use the Shell library
+ * as an interactive shell and trap extended instructions.
+ */
+
+#include <Shell.h>
+
+typedef Shell<16,16> BaseShell;
+class ExtendedShell : public BaseShell {
+public:
+  ExtendedShell(Stream& ios) : BaseShell(ios)
+  {}
+  virtual bool trap(char op)
+  {
+    m_ios.print(F("trap::op="));
+    m_ios.println(op);
+    return (true);
+  }
+};
+
+ExtendedShell shell(Serial);
+
+const int BUF_MAX = 64;
+char buf[BUF_MAX];
+char* bp = buf;
+
+void setup()
+{
+  Serial.begin(57600);
+  while (!Serial);
+  Serial.println(F("ShellTrap: started"));
+  shell.trace(true);
+}
+
+void loop()
+{
+  if (shell.read(bp)) {
+    shell.execute(buf);
+    bp = buf;
+  }
+}
